05d0d3fa28213da36bd8a4fe75297d9bb26b48a1
Jose Clavero <jose.clavero.anderica@gmail.com>
2024-10-21 22:27:24 +0200

    append parse sql

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/jose78/kubectl-fuck
 go 1.22.0
 
 require github.com/jose78/go-collections v0.0.0-20241019065928-96b487c772ab
+
+require github.com/xwb1989/sqlparser v0.0.0-20180606152119-120387863bf2 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	collection "github.com/jose78/go-collections"
+	"github.com/xwb1989/sqlparser"
+	"log"
 	"os"
 	"strings"
-
-	collection "github.com/jose78/go-collections"
 )
 
 func main() {
@@ -23,10 +24,10 @@ func mapper(item string) any {
 }
 
 // function to extract the query to be executed
-func selectQuery(nameVar string ) (string, error) {
-	envNameVar := fmt.Sprintf("K_FCK_%s"  ,  strings.ToUpper(nameVar))
+func selectQuery(nameVar string) (string, error) {
+	envNameVar := fmt.Sprintf("K_FCK_%s", strings.ToUpper(nameVar))
 	var predicate collection.Predicate[collection.Touple] = func(item collection.Touple) bool {
-		result := strings.Compare(item.Key.(string), envNameVar) == 0 
+		result := strings.Compare(item.Key.(string), envNameVar) == 0
 		return result
 	}
 
@@ -41,4 +42,44 @@ func selectQuery(nameVar string ) (string, error) {
 	} else {
 		return result[envNameVar], nil
 	}
+}
+
+type sqlContainer struct {
+	sqlSelect map[string]string
+	sqlFrom []string 
+	sqlWhere string 
+}
+
+
+func evaluateQuery(query string) (sqlContainer , error){
+	stmt, err := sqlparser.Parse(query)
+	if err != nil {
+		log.Fatalf("Error al parsear la consulta SQL: %v", err)
+	}
+
+	sql :=sqlContainer{}
+	switch stmt := stmt.(type) {
+	case *sqlparser.Select:
+		cols := map[string]string{}
+		for _, col := range stmt.SelectExprs {
+			col := sqlparser.String(col)
+			var key string
+			var value string
+			if upperNameItemSelect := strings.ToUpper(col);   strings.Contains(upperNameItemSelect, " AS ") {
+				key   = strings.Split(upperNameItemSelect, " AS ")[0]
+				value  = strings.Split(upperNameItemSelect, " AS ")[1]
+			} else {
+				colSplited := strings.Split(col, ".")
+				key = colSplited[len(colSplited) - 1]
+				value = col
+			}
+			cols[key] = value
+			sql.sqlSelect = cols
+		}
+		sql.sqlFrom =  strings.Split(sqlparser.String(stmt.From) , ",")
+		sql.sqlWhere = strings.ReplaceAll(sqlparser.String(stmt.Where ), " where ", "") 
+	default:
+		return sqlContainer{}, fmt.Errorf("")
+	}
+	return sql, nil
 }
